edbbceb99e40e0aee0beea28f85f0a4c0031ba85
Martin Ahindura <martinahindura@gmail.com>
2017-08-17 00:50:21 +0300

    added tests for utilities functions

--- flask_app/test/test_shoppinglist.py
+++ flask_app/test/test_shoppinglist.py
@@ -9,6 +9,7 @@ included:
 
 import unittest
 from app.classes import shopping
+from app.classes import utilities
 
 
 class UserTests(unittest.TestCase):
@@ -82,6 +83,8 @@ class UserTests(unittest.TestCase):
             'pas')
         self.assertRaises(TypeError, self.user.set_password,
             5)
+
+    # a user can delete/edit items that belong to shopping list he/she owns
         
  
 
@@ -99,9 +102,32 @@ class ShoppingListTests(unittest.TestCase):
 
     # an item can be added to Shopping list
     # an item can be deleted from a shopping list
-    # a shopping list's name can only be a string
-    # a shopping list's description can only be a string
-    # the creator of a shopping list is a User
+    
+    def test_title_is_string(self):
+        """
+        The title of the shopping list is 
+        of type string (str) 
+        """
+        self.assertRaises(TypeError, shopping.ShoppingList, 
+        5, 'A dummy description')
+        self.assertRaises(TypeError, self.shopping_list.set_title,
+            {'title':'string expected not dict'})
+ 
+    def test_description_is_string(self):
+        """
+        The description of a shopping list is 
+        of type string (str)
+        """
+        self.assertRaises(TypeError, shopping.ShoppingList, 
+        'groceries', 55)
+        self.assertRaises(TypeError, self.shopping_list.set_description,
+            {'description':'string expected not dict'})
+
+    def test_creator_is_right_type(self):
+        """
+        A creator of a shopping list should be
+        an instance of the User class only (not even None)
+        """
 
 
 
@@ -121,6 +147,51 @@ class ShoppingItemTests(unittest.TestCase):
     # an item's name can only be a string
     # an item's unit can only be a string
     # the item belongs to a shopping list
+
+
+class UtilitiesTests(unittest.TestCase):
+    """
+    Class to test the functionality of the functions
+    in utilities.py
+    """
+    
+    def test_check_type_return(self):
+        """
+        check_type returns True when types are the same
+        check_type raises TypeError when the types are different
+        check_type raises an error when the type_object is not a type
+        """
+        self.assertTrue(utilities.check_type('girl', str))
+        self.assertRaises(TypeError, utilities.check_type, 5, bool)
+        self.assertRaises(ValueError, utilities.check_type, 5, 9)
+    
+    def test_check_email_format(self):
+        """
+        check_email_formart returns True when email is right format
+        check_email_format raises a ValueError when email format is wrong
+        check_email_format raises a TypeError when email is not string
+        """
+        self.assertTrue(utilities.check_email_format('tom@example.com'))
+        self.assertRaises(ValueError, utilities.check_email_format,
+                        'tomexample.com')
+        self.assertRaises(TypeError, utilities.check_email_format,
+                        56)
+
+    def test_check_password_format(self):
+        """
+        check_password_format returns True for a String of more characters
+        check_password_format raises a ValueError when password string
+        is of length less than min_length (default 6)
+        check_password_format raises a TypeError when password is not string
+        check_password_format raises a TypeError when min_length is not an int
+        """
+        self.assertTrue(utilities.check_password_format('rango679kiy'))
+        self.assertRaises(ValueError, utilities.check_password_format, 'present',
+                        10)
+        self.assertRaises(TypeError, utilities.check_password_format, 43.5)
+        self.assertRaises(TypeError, utilities.check_password_format, 'password',
+                        '34r')
+
 
 
 if __name__ == '__main__':
